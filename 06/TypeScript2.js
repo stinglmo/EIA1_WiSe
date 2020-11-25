289c76b04c243d39f6dde38f7befad60aed0cb7c
MONA <mona.stingl@hs-furtwangen.de>
2020-11-25 18:07:24 +0100

    Letzte Änderungen Unterschrift Kontinent

--- 06/TypeScript2.js
+++ 06/TypeScript2.js
@@ -45,7 +45,8 @@ var NAME;
         document.getElementById("WachstumsrateProzent").innerHTML = ProzentualeVeränderung.toFixed(2) + "%";
         document.getElementById("WachstumsrateAbsolut").innerHTML = VeränderungCO2.toFixed(2);
         document.querySelector(".chart").setAttribute('style', 'height:' + (Wert2018 / Gesamtemission * 100).toFixed(2) + '%');
-        document.querySelector(".titleRegion").innerHTML = Kontinent;
+        document.getElementById("#titleRegion").innerHTML = Kontinent;
+        document.getElementById("#titleRegionU").innerHTML = Kontinent;
         document.querySelector(Kontinent).setAttribute('class', 'active');
     }
     // Europa //
