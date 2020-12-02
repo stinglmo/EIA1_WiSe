d9c2750dd3a4a9f3e5eb920e9b07c7ca31af30fa
MONA <mona.stingl@hs-furtwangen.de>
2020-12-02 13:27:13 +0100

    1. Aufgabe

--- 07/TypeScript.js
+++ 07/TypeScript.js
@@ -1,41 +1,114 @@
-// Drum Pad //
-var sound = new Audio('sample.mp3');
-sound.play();
+// Drum Pad - Sounds //
+var Sound1 = new Audio('./assets/A.mp3');
+var Sound2 = new Audio('./assets/C.mp3');
+var Sound3 = new Audio('./assets/F.mp3');
+var Sound4 = new Audio('./assets/G.mp3');
+var Sound5 = new Audio('./assets/hihat.mp3');
+var Sound6 = new Audio('./assets/kick.mp3');
+var Sound7 = new Audio('./assets/laugh-1.mp3');
+var Sound8 = new Audio('./assets/laugh-2.mp3');
+var Sound9 = new Audio('./assets/snare.mp3');
+// Array //
+var ALLSOUNDS = ['Sound1', 'Sound2', 'Sound3', 'Sound4', 'Sound5', 'Sound6', 'Sound7', 'Sound8', 'Sound9'];
 // Funktion //
 function playSample() {
-    document.getElementById('Button');
+    document.querySelector('.button').innerHTML;
 }
 // Funktionsaufrufe //
 document.getElementById('Nummer1').addEventListener('click', function () {
-    playSample();
+    Sound1.play();
 });
 document.getElementById('Nummer2').addEventListener('click', function () {
-    playSample;
+    Sound2.play();
 });
 document.getElementById('Nummer3').addEventListener('click', function () {
-    playSample;
+    Sound3.play();
 });
 document.getElementById('Nummer4').addEventListener('click', function () {
-    playSample;
+    Sound4.play();
 });
 document.getElementById('Nummer5').addEventListener('click', function () {
-    playSample;
+    Sound5.play();
 });
 document.getElementById('Nummer6').addEventListener('click', function () {
-    playSample;
+    Sound6.play();
 });
 document.getElementById('Nummer7').addEventListener('click', function () {
-    playSample;
+    Sound7.play();
 });
 document.getElementById('Nummer8').addEventListener('click', function () {
-    playSample;
+    Sound8.play();
 });
 document.getElementById('Nummer9').addEventListener('click', function () {
-    playSample;
+    Sound9.play();
 });
 // Drum Machine //
+function Intervall() {
+    document.querySelector('#play-button').innerHTML;
+}
 setInterval(function () {
-    document.getElementsByClassName('Nummer1');
+    Sound5;
+    Sound6;
+    Sound5;
+    Sound6;
+    Sound9;
+    Sound9;
+    Sound5;
+    Sound6;
+    Sound5;
+    Sound6;
+    Sound9;
+    Sound9;
+    Sound5;
+    Sound6;
+    Sound5;
+    Sound6;
+    Sound9;
+    Sound9;
+    Sound5;
+    Sound6;
+    Sound5;
+    Sound6;
+    Sound9;
+    Sound9;
+    Sound5;
+    Sound6;
+    Sound5;
+    Sound6;
+    Sound9;
+    Sound9;
+    Sound5;
+    Sound6;
+    Sound5;
+    Sound6;
+    Sound9;
+    Sound9;
+    Sound5;
+    Sound6;
+    Sound5;
+    Sound6;
+    Sound9;
+    Sound9;
+    Sound5;
+    Sound6;
+    Sound5;
+    Sound6;
+    Sound9;
+    Sound9;
+    Sound5;
+    Sound6;
+    Sound5;
+    Sound6;
+    Sound9;
+    Sound9;
+    Sound5;
+    Sound6;
+    Sound5;
+    Sound6;
+    Sound9;
+    Sound9;
 }, 500);
-// document.getElementById('play-button').setAttribute('click')
+document.getElementById('play-button').addEventListener('click', function () {
+    setInterval;
+});
 //# sourceMappingURL=TypeScript.js.map
--- 07/TypeScript.ts
+++ 07/TypeScript.ts
@@ -1,52 +1,70 @@
 
-// Drum Pad //
+// Drum Pad - Sounds //
 
-var sound:HTMLAudioElement = new Audio('sample.mp3');
-sound.play();
+const Sound1:HTMLAudioElement = new Audio('./assets/A.mp3');
 
+const Sound2:HTMLAudioElement = new Audio('./assets/C.mp3');
+
+const Sound3:HTMLAudioElement = new Audio('./assets/F.mp3');
+
+const Sound4:HTMLAudioElement = new Audio('./assets/G.mp3');
+
+const Sound5:HTMLAudioElement = new Audio('./assets/hihat.mp3');
+
+const Sound6:HTMLAudioElement = new Audio('./assets/kick.mp3');
+
+const Sound7:HTMLAudioElement = new Audio('./assets/laugh-1.mp3');
+
+const Sound8:HTMLAudioElement = new Audio('./assets/laugh-2.mp3');
+
+const Sound9:HTMLAudioElement = new Audio('./assets/snare.mp3');
+
+// Array //
+
+const ALLSOUNDS:any [] = ['Sound1', 'Sound2', 'Sound3', 'Sound4', 'Sound5', 'Sound6', 'Sound7', 'Sound8', 'Sound9']
 
 // Funktion //
 
 function playSample(): any {
-    document.getElementById('Button')
+    document.querySelector('.button').innerHTML 
 }
 
 // Funktionsaufrufe //
 
 document.getElementById('Nummer1').addEventListener('click', function() {
-    playSample()
+    Sound1.play();
 })
 
 document.getElementById('Nummer2').addEventListener('click', function() {
-    playSample
+    Sound2.play();
 })
 
 document.getElementById('Nummer3').addEventListener('click', function() {
-    playSample
+    Sound3.play();
 })
 
 document.getElementById('Nummer4').addEventListener('click', function() {
-    playSample
+    Sound4.play();
 })
 
 document.getElementById('Nummer5').addEventListener('click', function() {
-    playSample
+    Sound5.play();
 })
 
 document.getElementById('Nummer6').addEventListener('click', function() {
-    playSample
+    Sound6.play();
 })
 
 document.getElementById('Nummer7').addEventListener('click', function() {
-    playSample
+    Sound7.play();
 })
 
 document.getElementById('Nummer8').addEventListener('click', function() {
-    playSample
+    Sound8.play();
 })
 
 document.getElementById('Nummer9').addEventListener('click', function() {
-    playSample
+    Sound9.play();
 })
 
 
@@ -55,8 +73,18 @@ document.getElementById('Nummer9').addEventListener('click', function() {
 
 // Drum Machine //
 
+function Intervall(): any {
+    document.querySelector('#play-button').innerHTML 
+}
+
 setInterval(function() {
-    document.getElementsByClassName('Nummer1')
+    Sound5; Sound6; Sound5; Sound6; Sound9; Sound9; Sound5; Sound6; Sound5; Sound6; Sound9; Sound9;
+    Sound5; Sound6; Sound5; Sound6; Sound9; Sound9; Sound5; Sound6; Sound5; Sound6; Sound9; Sound9;
+    Sound5; Sound6; Sound5; Sound6; Sound9; Sound9; Sound5; Sound6; Sound5; Sound6; Sound9; Sound9;
+    Sound5; Sound6; Sound5; Sound6; Sound9; Sound9; Sound5; Sound6; Sound5; Sound6; Sound9; Sound9;
+    Sound5; Sound6; Sound5; Sound6; Sound9; Sound9; Sound5; Sound6; Sound5; Sound6; Sound9; Sound9;
    }, 500);
 
- // document.getElementById('play-button').setAttribute('click')
+ document.getElementById('play-button').addEventListener('click', function() {
+   setInterval
+   });
