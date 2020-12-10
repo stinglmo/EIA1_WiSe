c26fc58612c4b91aff3c43e6a985676531629a9a
MONA <mona.stingl@hs-furtwangen.de>
2020-12-10 15:53:35 +0100

    Fast fertig *_*

--- 08/TypeScript.js
+++ 08/TypeScript.js
@@ -18,83 +18,113 @@ var NAME;
     // Funktionsaufrufe //
     document.getElementById("Nummer1").addEventListener("click", function () {
         playSample(0);
+        recBeat(0);
     });
     document.getElementById("Nummer2").addEventListener("click", function () {
         playSample(1);
+        recBeat(1);
     });
     document.getElementById("Nummer3").addEventListener("click", function () {
         playSample(2);
+        recBeat(2);
     });
     document.getElementById("Nummer4").addEventListener("click", function () {
         playSample(3);
+        recBeat(3);
     });
     document.getElementById("Nummer5").addEventListener("click", function () {
         playSample(4);
+        recBeat(4);
     });
     document.getElementById("Nummer6").addEventListener("click", function () {
         playSample(5);
+        recBeat(5);
     });
     document.getElementById("Nummer7").addEventListener("click", function () {
         playSample(6);
+        recBeat(6);
     });
     document.getElementById("Nummer8").addEventListener("click", function () {
         playSample(7);
+        recBeat(7);
     });
     document.getElementById("Nummer9").addEventListener("click", function () {
         playSample(8);
+        recBeat(8);
     });
-    // Drum Machine - SetIntervall sorgt für Wiederholung (kontinuierlich)
-    var SEQ = ["./assets/hihat.mp3", "./assets/kick.mp3", "./assets/snare.mp3"];
-    var INDEXSEQ = 0;
-    function Intervall() {
-        setInterval(function () {
-            var SOUND = new Audio(SEQ[INDEXSEQ]);
-            SOUND.play();
-            INDEXSEQ = INDEXSEQ + 1;
-        }, 500);
-    }
-    document.getElementById("play").addEventListener("click", function () {
-        Intervall();
-    });
+    // Warum werden 8 und 9 nicht abgespielt
     // Array Buttons //
     var button = [];
     button[0] = document.getElementById("play");
     button[1] = document.getElementById("stop");
     button[2] = document.getElementById("micro");
     button[3] = document.getElementById("trash");
-    // Funktionen Buttons - Damit man hin und her switchen kann //
+    // Leeres Array - um Beat drinnen zu speichern
+    var leeresArray = [];
+    // Funktion - Record - Button 
+    var recording;
+    function recBeat(index) {
+        if (recording == true) {
+            leeresArray.push(index); // Index --> Beat wird im Array gespeichert
+        }
+    }
+    // Funktion Schleife - SetIntervall sorgt für Wiederholung (kontinuierlich) - 400ms
+    // Intervall als Variable - Dann wird das Intervall in der Variable gespeichert und durch clearIntervall wird die Variable wieder geleehrt.
+    var interval;
+    var x = 0;
+    function loop(recorded) {
+        if (recorded == true) {
+            interval = setInterval(function () {
+                if ((x < leeresArray.length)) {
+                    playSample(leeresArray[x]); // Die x-te Stelle meines Arrays wird abgespielt!
+                    x++;
+                }
+                else {
+                    x = 0;
+                }
+            }, 400);
+        }
+        else {
+            clearInterval(interval);
+        }
+    }
+    // Funktionen Buttons - Damit man zwischen Play und Stop hin und her switchen kann //
     function switchclasses(index1, index2) {
         button[index1].classList.add("is-hidden");
         button[index2].classList.remove("is-hidden");
     }
-    // Play Button - Sobald ich auf play drücke, bekommt es die Klasse "is-hidden", und ist somit nicht mehr sichtbar!
+    // Play Button - Sobald ich auf play drücke, bekommt 
+    // 1. mein Play-Button die Klasse "is-hidden", und ist somit nicht mehr sichtbar!
+    // 2. der if - Codeblock von meiner loop - Funktion wird ausgeführt --> Meine Beatschleife wird in der Variablen "interval" gespeichert!
     button[0].addEventListener("click", function () {
         switchclasses(0, 1);
+        loop(true);
     });
-    // Stop - Button - Sobald ich auf stop drücke, wird die im HTML-Dokument festgelegte Klasse "is-hidden" entfernt und das Icon ist damit sichtbar!
+    // Stop - Button - Sobald ich auf stop drücke, wird 
+    // 1. die Funktion switchclasses ausgeführt --> die im HTML-Dokument festgelegte Klasse "is-hidden" entfernt und das Icon ist damit sichtbar!
+    // 2. der else - Codeblock von meiner loop - Funktion ausgeführt --> Die Variable in der ich die Beatschleife gespeichert habe, wird (durch clearInterval) gelehrt!
     button[1].addEventListener("click", function () {
         switchclasses(1, 0);
+        loop(false);
     });
-    // Record - Button 
-    // Leeres Array - um Beat drinnen zu speichern
-    var leeresArray = [];
-    function recBeat(index) {
-        leeresArray.push(sound[index]);
-    }
-    console.log(leeresArray);
     // Record - Button - Changecolour 
     button[2].addEventListener("click", function () {
         if (button[2].classList.contains("inactive")) {
             button[2].classList.remove("inactive");
             button[2].classList.add("active");
+            // recording = true --> in Funktion recBeat habe ich festgelegt, dass dann die Beats im leeren Array gespeichert werden!
+            recording = true;
         }
         else {
             button[2].classList.remove("active");
             button[2].classList.add("inactive");
+            recording = false;
         }
     });
     // Delete - Button 
     button[3].addEventListener("click", function () {
+        leeresArray.length = 0;
+        console.log(leeresArray.length);
     });
 })(NAME || (NAME = {}));
 //# sourceMappingURL=TypeScript.js.map
--- 08/TypeScript.ts
+++ 08/TypeScript.ts
@@ -25,62 +25,53 @@ namespace NAME {
 
     document.getElementById("Nummer1").addEventListener("click", function (): void {
         playSample(0);
+        recBeat(0);
     
     });
 
     document.getElementById("Nummer2").addEventListener("click", function (): void {
         playSample(1);
+        recBeat(1);
     });
 
     document.getElementById("Nummer3").addEventListener("click", function (): void {
         playSample(2);
+        recBeat(2);
     });
 
     document.getElementById("Nummer4").addEventListener("click", function (): void {
         playSample(3);
+        recBeat(3);
     });
 
     document.getElementById("Nummer5").addEventListener("click", function (): void {
         playSample(4);
+        recBeat(4);
     });
 
     document.getElementById("Nummer6").addEventListener("click", function (): void {
         playSample(5);
+        recBeat(5);
     });
 
     document.getElementById("Nummer7").addEventListener("click", function (): void {
         playSample(6);
+        recBeat(6);
     });
 
     document.getElementById("Nummer8").addEventListener("click", function (): void {
         playSample(7);
+        recBeat(7);
     });
 
     document.getElementById("Nummer9").addEventListener("click", function (): void {
         playSample(8);
+        recBeat(8);
     });
 
-    // Drum Machine - SetIntervall sorgt für Wiederholung (kontinuierlich)
-
-    let SEQ: string[] = ["./assets/hihat.mp3", "./assets/kick.mp3", "./assets/snare.mp3"];
-
-    let INDEXSEQ: number = 0;
+    // Warum werden 8 und 9 nicht abgespielt
 
 
-    function Intervall(): void {
-        setInterval(function (): void {
-            let SOUND: HTMLAudioElement = new Audio(SEQ[INDEXSEQ]);
-            SOUND.play();
-
-            INDEXSEQ = INDEXSEQ + 1;
-
-        },          500);
-    }
-
-
-    document.getElementById("play").addEventListener("click", function (): void {
-        Intervall();
-    });
 
     // Array Buttons //
 
@@ -91,7 +82,43 @@ namespace NAME {
     button[2] = document.getElementById("micro");
     button[3] = document.getElementById("trash");
 
-    // Funktionen Buttons - Damit man hin und her switchen kann //
+     // Leeres Array - um Beat drinnen zu speichern
+
+    let leeresArray: number[] = []; 
+
+     // Funktion - Record - Button 
+
+    let recording: boolean; 
+
+    function recBeat(index: number): void {
+         if (recording == true) {
+            leeresArray.push(index); // Index --> Beat wird im Array gespeichert
+         }
+    }
+        
+    // Funktion Schleife - SetIntervall sorgt für Wiederholung (kontinuierlich) - 400ms
+    // Intervall als Variable - Dann wird das Intervall in der Variable gespeichert und durch clearIntervall wird die Variable wieder geleehrt.
+    
+    let interval: number;
+    let x: number = 0;
+
+    function loop(recorded: boolean): void {
+        if (recorded == true) {
+            interval = setInterval(function(): void {
+                
+                    if ((x < leeresArray.length)) {
+                        playSample(leeresArray[x]); // Die x-te Stelle meines Arrays wird abgespielt!
+                        x++;
+                    } else {
+                        x = 0;
+                    }
+            },                     400);
+        } else {
+            clearInterval(interval);
+        }
+    }
+
+    // Funktionen Buttons - Damit man zwischen Play und Stop hin und her switchen kann //
 
     function switchclasses(index1: number, index2: number): void {
         button[index1].classList.add("is-hidden");
@@ -99,35 +126,25 @@ namespace NAME {
 
     }
 
-    // Play Button - Sobald ich auf play drücke, bekommt es die Klasse "is-hidden", und ist somit nicht mehr sichtbar!
+    // Play Button - Sobald ich auf play drücke, bekommt 
+    // 1. mein Play-Button die Klasse "is-hidden", und ist somit nicht mehr sichtbar!
+    // 2. der if - Codeblock von meiner loop - Funktion wird ausgeführt --> Meine Beatschleife wird in der Variablen "interval" gespeichert!
 
     button[0].addEventListener("click", function (): void {
         switchclasses(0, 1);
+        loop(true);
 
     });
 
-    // Stop - Button - Sobald ich auf stop drücke, wird die im HTML-Dokument festgelegte Klasse "is-hidden" entfernt und das Icon ist damit sichtbar!
+    // Stop - Button - Sobald ich auf stop drücke, wird 
+    // 1. die Funktion switchclasses ausgeführt --> die im HTML-Dokument festgelegte Klasse "is-hidden" entfernt und das Icon ist damit sichtbar!
+    // 2. der else - Codeblock von meiner loop - Funktion ausgeführt --> Die Variable in der ich die Beatschleife gespeichert habe, wird (durch clearInterval) gelehrt!
 
     button[1].addEventListener("click", function (): void {
         switchclasses(1, 0);
+        loop(false);
 
     });
-
-   
-    
-
-    // Record - Button 
-
-     // Leeres Array - um Beat drinnen zu speichern
-
-    let leeresArray: any = []; 
-
-    function recBeat(index: number): void {
-        leeresArray.push(sound[index]);
-        }
-       
-    console.log(leeresArray);
-    
 
     // Record - Button - Changecolour 
 
@@ -135,25 +152,22 @@ namespace NAME {
         if (button[2].classList.contains("inactive")) {
             button[2].classList.remove("inactive");
             button[2].classList.add("active");
-            
-    
+            // recording = true --> in Funktion recBeat habe ich festgelegt, dass dann die Beats im leeren Array gespeichert werden!
+            recording = true; 
         } else {
             button[2].classList.remove("active");
             button[2].classList.add("inactive");
+            recording = false;
         }
     });
-
-    
-   
-   
-   
-   
-   
+ 
    // Delete - Button 
 
-button[3].addEventListener("click", function(): void {
+    button[3].addEventListener("click", function(): void {
+        leeresArray.length = 0;
+        console.log(leeresArray.length);
         
-    })
+    });
 
 
 }
