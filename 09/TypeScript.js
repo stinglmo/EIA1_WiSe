8ed4e9439a1af784a7169c4ef977f8b6ef60282b
MONA <mona.stingl@hs-furtwangen.de>
2020-12-16 14:14:59 +0100

    Listenelemente anzeigen lassen! Ticktask und deletetask separiert
    - Delete-button funktioniert jetzt, nur kann ich wieder nur noch ein Listenelement hinzufügen

--- 09/TypeScript.js
+++ 09/TypeScript.js
@@ -3,7 +3,6 @@ loadEvents();
 function loadEvents() {
     document.querySelector("form").addEventListener("submit", submit); // Die Funktion submit wird aufgerufen - Absenden der ToDo`s wird möglich gemacht!
     document.getElementById("clear").addEventListener("click", clearList); // Die Funktion clearList wird bei Klick aufgerufen - Die gesamte Liste mit allen ToDo`s wird gelöscht! 
-    document.querySelector("ul").addEventListener("click", deleteOrTick); // Die Funktion  deleteOrTick wird aufgerufen - Man kann entweder löschen oder ankreuzen!
 }
 // Funktion Absenden
 var input = document.querySelector("input");
@@ -14,37 +13,37 @@ function submit(e) {
     input.value = " "; // Das Textfeld wird wieder leer!
 }
 // ToDos hinzufügen - // Die let - Variablen, schreibe ich in die Funktion rein, da sie nur in dem Bereich gültig sein sollen (im Deklarationsbereich - Bei addTask-Funktion also erst erscheinen sollen)
+var doTodoElements = document.getElementsByClassName("doTodo");
+var deleteTodoElements = document.getElementsByClassName("delete");
 var taskboard = document.querySelector(".tasksBoard");
-// let listelements: HTMLElement = document.querySelector(".ListElements"); ????
 function addTask(task) {
     var ul = document.querySelector("ul");
     var li = document.createElement("li");
-    if (taskboard.classList.contains("is-hidden"))
+    if (taskboard.classList.contains("is-hidden")) {
         taskboard.classList.remove("is-hidden"); // Aufgabenboard erscheint
-    // listelements.innerHTML = listNumber + " to-dos left"; )))
-    li.innerHTML = "<span class=\"delete\"><i class=\"fas fa-trash-alt\"></i></span><input type=\"checkbox\"><label>" + task + "</label>"; // Mit dem Dollerzeichen bestimme ich, dass sich die Checkbox ausschließlich auf dieses Label bezieht.
-    ul.appendChild(li); // Damit immer ein neues hinzugefügt werden kann!
-}
-// Löschen der gesamten Liste
-var ul = document.querySelector("ul");
-function clearList() {
-    ul.innerHTML = " ";
-}
-// Ankreuzen oder Ankreuzen rückgängig machen
-function deleteOrTick(e) {
-    if (e.target.className == "delete")
-        deleteTask(e);
-    else {
-        tickTask(e);
+        li.innerHTML = "<span class=\"delete\"><i class=\"fas fa-trash-alt\"></i></span><input class=\"doTodo\" type=\"checkbox\"><label>" + task + "</label>"; // Mit dem Dollerzeichen bestimme ich, dass sich die Checkbox ausschließlich auf dieses Label bezieht.
+        ul.appendChild(li); // Damit immer ein neues hinzugefügt werden kann!
+        for (var i = 0; i < doTodoElements.length; i++) {
+            doTodoElements[i].addEventListener("click", tickTask, false);
+            numberoflistelement();
+        }
+        for (var i = 0; i < doTodoElements.length; i++) {
+            deleteTodoElements[i].addEventListener("click", deleteTask, false);
+            numberoflistelement();
+        }
     }
+}
+// Anzeigen, wie viele todos noch existieren
+function numberoflistelement() {
+    document.getElementById("number").innerHTML = "elements left: " + doTodoElements.length;
 }
 // ToDo löschen - // DOM - Manipulation durch Document.adoptNode()!
 // parentNode und nextSilbling greifen auf Elemente zu, 
 // die nicht direkt durch Methoden wie getElementById, getElementsByName 
 // oder querySelector erreicht werden und finden jedes Element im DOM auch ohne ID und class-Attribut.
 // Node = Schnittstelle -> Typen werden vererbt.
-function deleteTask(e) {
-    var remove = e.target.parentNode;
+function deleteTask() {
+    var remove = this.parentNode;
     var parentNode = remove.parentNode;
     parentNode.removeChild(remove); // Das nächste Kind wird entfernt
 }
@@ -56,8 +55,13 @@ function tickTask(e) {
         task.style.color = "#ff0000";
     }
     else {
-        task.style.textDecoration = "none";
+        task.style.textDecoration = "";
         task.style.color = "#2f4f4f";
     }
+}
+// Löschen der gesamten Liste
+var ul = document.querySelector("ul");
+function clearList() {
+    ul.innerHTML = " ";
 }
 //# sourceMappingURL=TypeScript.js.map
--- 09/TypeScript.ts
+++ 09/TypeScript.ts
@@ -5,7 +5,6 @@ loadEvents();
 function loadEvents(): void {
   document.querySelector("form").addEventListener("submit", submit); // Die Funktion submit wird aufgerufen - Absenden der ToDo`s wird möglich gemacht!
   document.getElementById("clear").addEventListener("click", clearList); // Die Funktion clearList wird bei Klick aufgerufen - Die gesamte Liste mit allen ToDo`s wird gelöscht! 
-  document.querySelector("ul").addEventListener("click" , deleteOrTick); // Die Funktion  deleteOrTick wird aufgerufen - Man kann entweder löschen oder ankreuzen!
 
 }
 
@@ -22,37 +21,37 @@ function submit(e: Event): void {
 
 // ToDos hinzufügen - // Die let - Variablen, schreibe ich in die Funktion rein, da sie nur in dem Bereich gültig sein sollen (im Deklarationsbereich - Bei addTask-Funktion also erst erscheinen sollen)
 
+const doTodoElements: HTMLCollection = document.getElementsByClassName("doTodo");
+const deleteTodoElements: HTMLCollection = document.getElementsByClassName("delete");
+
 let taskboard: HTMLElement = document.querySelector(".tasksBoard");
-// let listelements: HTMLElement = document.querySelector(".ListElements"); ????
 
 function addTask(task: string): void {
   let ul: HTMLUListElement = document.querySelector("ul");
   let li: HTMLLIElement = document.createElement("li");
-  if (taskboard.classList.contains("is-hidden"))
+  if (taskboard.classList.contains("is-hidden")) {
   taskboard.classList.remove("is-hidden"); // Aufgabenboard erscheint
-  // listelements.innerHTML = listNumber + " to-dos left"; )))
-  li.innerHTML = `<span class="delete"><i class="fas fa-trash-alt"></i></span><input type="checkbox"><label>${task}</label>`; // Mit dem Dollerzeichen bestimme ich, dass sich die Checkbox ausschließlich auf dieses Label bezieht.
+  li.innerHTML = `<span class="delete"><i class="fas fa-trash-alt"></i></span><input class="doTodo" type="checkbox"><label>${task}</label>`; // Mit dem Dollerzeichen bestimme ich, dass sich die Checkbox ausschließlich auf dieses Label bezieht.
   ul.appendChild(li); // Damit immer ein neues hinzugefügt werden kann!
-  
 
-}
-
-// Löschen der gesamten Liste
-
-let ul: HTMLUListElement = document.querySelector("ul");
-
-function clearList(): void {
-  ul.innerHTML = " ";
-}
-
-// Ankreuzen oder Ankreuzen rückgängig machen
-
-function deleteOrTick(e): void {
-  if (e.target.className == "delete")
-    deleteTask(e);
-  else {
-    tickTask(e);
+  for (let i: number = 0; i < doTodoElements.length; i++) {
+    doTodoElements[i].addEventListener("click", tickTask, false);
+    numberoflistelement();
   }
+
+  for (let i: number = 0; i < doTodoElements.length; i++) {
+    deleteTodoElements[i].addEventListener("click", deleteTask, false);
+    numberoflistelement();
+  }
+
+  }
+  
+}
+
+// Anzeigen, wie viele todos noch existieren
+
+function numberoflistelement(): void {
+  document.getElementById("number").innerHTML = "elements left: " + doTodoElements.length;
 }
 
 // ToDo löschen - // DOM - Manipulation durch Document.adoptNode()!
@@ -61,8 +60,8 @@ function deleteOrTick(e): void {
 // oder querySelector erreicht werden und finden jedes Element im DOM auch ohne ID und class-Attribut.
 // Node = Schnittstelle -> Typen werden vererbt.
 
-function deleteTask(e): void {
-  let remove: HTMLElement = e.target.parentNode; 
+function deleteTask(): void {
+  let remove: HTMLElement = this.parentNode; 
   let parentNode: Node = remove.parentNode;
   parentNode.removeChild(remove); // Das nächste Kind wird entfernt
 }
@@ -75,7 +74,15 @@ function tickTask(e): void {
     task.style.textDecoration = "line-through"; // ToDo wird durchgestrichen. Ist das okay, wenn ich es hier mache? Oder mit CSS-Klassen?
     task.style.color = "#ff0000";
   } else {
-    task.style.textDecoration = "none";
+    task.style.textDecoration = "";
     task.style.color = "#2f4f4f";
   }
+}
+
+// Löschen der gesamten Liste
+
+let ul: HTMLUListElement = document.querySelector("ul");
+
+function clearList(): void {
+  ul.innerHTML = " ";
 }
